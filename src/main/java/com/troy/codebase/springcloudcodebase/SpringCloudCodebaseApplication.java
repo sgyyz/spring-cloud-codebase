3c09ace26a509b08865c384183f35db615cee472
Yongzhi Yang <yongzhi.yang@57blocks.io>
2018-08-19 10:57:45 +0800

    Init commit

---
+++ src/main/java/com/troy/codebase/springcloudcodebase/SpringCloudCodebaseApplication.java
@@ -0,0 +1,12 @@
+package com.troy.codebase.springcloudcodebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCodebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudCodebaseApplication.class, args);
+	}
+}
