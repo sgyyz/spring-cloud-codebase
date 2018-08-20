762cf1ab72713cb2101b0d22dfe20081e395c313
Troy Young <itnote.yyz@gmail.com>
2018-08-20 09:43:34 +0800

    Update Discovery Main Class Name and Add Context Loads Unit Tests

--- discovery/src/main/java/com/troy/codebase/DiscoveryApplication.java
+++ discovery/src/main/java/com/troy/codebase/DiscoveryApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryApp {
+public class DiscoveryApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DiscoveryApp.class, args);
+    SpringApplication.run(DiscoveryApplication.class, args);
   }
 }
