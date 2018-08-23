f217916a6c4f8ad3ca84d57bcb889e9713d9ad20
Troy Young <itnote.yyz@gmail.com>
2018-08-23 15:58:49 +0800

    Remove @EnableDiscoveryClient annotation by using auto-config

--- oauth/src/main/java/com/troy/codebase/OAuthApplication.java
+++ oauth/src/main/java/com/troy/codebase/OAuthApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class OAuthApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(WebApplication.class, args);
+    SpringApplication.run(OAuthApplication.class, args);
   }
 }
